a78eb2549b69d557d9f6b4aa8301ccab74acf97b
Aaron McAllister <aaron.mcallister@terminalfour.com>
2026-03-17 17:38:08 +0000

    testing zed with git

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use std::io;
-
+// testing git
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{
+    DefaultTerminal, Frame,
     buffer::Buffer,
     layout::Rect,
     style::Stylize,
     symbols::border,
     text::{Line, Text},
     widgets::{Block, Paragraph, Widget},
-    DefaultTerminal, Frame,
 };
 
 fn main() -> io::Result<()> {
@@ -18,7 +18,7 @@ fn main() -> io::Result<()> {
 struct Tile {
     color: String,
     clicked: bool,
-    matched: bool
+    matched: bool,
 }
 
 #[derive(Debug, Default)]
@@ -29,7 +29,6 @@ pub struct App {
 }
 
 impl App {
-
     /// runs the application's main loop until the user quits
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
         while !self.exit {
@@ -69,20 +68,12 @@ impl App {
 impl Widget for &App {
     fn render(self, area: Rect, buf: &mut Buffer) {
         let title = Line::from("Matching Game".bold());
-        let instructions = Line::from(vec![
-            " Quit ".into(),
-            "<Q> ".blue().bold(),
-        ]);
+        let instructions = Line::from(vec![" Quit ".into(), "<Q> ".blue().bold()]);
         let block = Block::bordered()
             .title(title.centered())
             .title_bottom(instructions.centered())
             .border_set(border::THICK);
 
-
-        Paragraph::new("")
-            .centered()
-            .block(block)
-            .render(area, buf);
+        Paragraph::new("").centered().block(block).render(area, buf);
     }
 }
-
